d88a9506090f9cf76a0c08accfea52dd9372f674
lemish11 <119430087+lemish11@users.noreply.github.com>
2023-11-22 12:48:27 +0300

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -77,3 +77,7 @@ set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
 :end
 @rem End local scope for the variables with windows NT shell
 if %ERRORLEVEL% equ 0 goto mainEnd
+
+:fail
+rem Set variable GRADLE_EXIT_CONSOLE if you need the _script_ return code instead of
+rem the _cmd.exe /c_ return code!
